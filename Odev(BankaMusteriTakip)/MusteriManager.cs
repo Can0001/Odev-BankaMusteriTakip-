16ee29c98ee9a073f9c90ae1aa3f473c23249b02
CanAs <CanAs@DESKTOP-2HO4PK8>
2022-08-26 10:00:38 +0300

    Add project files.

---
+++ Odev(BankaMusteriTakip)/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_BankaMusteriTakip_
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Başarılı Bir Şekilde Eklendi");
+            Console.WriteLine("Müşteri Adı Ve Soyadı : " + musteri.Adi + " "+ musteri.Soyadi);
+            Console.WriteLine("Müşteri HesapNo: "+ musteri.Hesapno);
+            Console.WriteLine("-------------------------------------");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Başarılı Bir Şekilde Silindi");
+            Console.WriteLine("Müşteri Adı Ve Soyadı : " + musteri.Adi +" "+ musteri.Soyadi);
+            Console.WriteLine("Müşteri HesapNo: " + musteri.Hesapno);
+            Console.WriteLine("-------------------------------------");
+        }
+
+    }
+}
---
+++ Odev(BankaMusteriTakip)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Odev_BankaMusteriTakip_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Can";
+            musteri1.Soyadi="Tüer";
+            musteri1.Hesapno = 123;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Nazlı";
+            musteri2.Soyadi = "Tüer";
+            musteri2.Hesapno = 222;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Şükufe";
+            musteri3.Soyadi = "Nazlı";
+            musteri3.Hesapno = 111;
+
+            Musteri[] musteriler=new Musteri[] { musteri1, musteri2,musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            
+            musteriManager.Ekle(musteri2);
+
+            musteriManager.Sil(musteri3);
+
+        }
+    }
+}
